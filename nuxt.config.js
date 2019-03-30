cb17d859187ef7791256af2779519dad72d575d6
Helicoir <shogo,kobayashi@wasedasai.net>
2019-03-30 22:36:47 +0900

    retry(setting ogp, change matsuridon.jpg)

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '早稲田祭2019運営スタッフ 新歓公式サイト' },
+      { hid: 'og:image', property: 'og:image', content: '/ogp.png' },
+      { hid: "og:title", property: 'og:title', content: "早稲田祭2019運営スタッフ 新歓公式サイト"},
+      { hid: 'og:type', property: 'og:type', content: 'website' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
